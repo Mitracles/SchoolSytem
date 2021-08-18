d2a8295a2cf42db53935b55846995d3a227525b2
Mitracles <michaelmitra45@gmail.com>
2021-08-18 22:34:28 +0800

    add first

---
+++ src/People/Teachers.java
@@ -0,0 +1,50 @@
+package People;
+
+public class Teachers {
+    //Setters & Getters
+    private String name;
+    private int facultyNo;
+    private char address;
+    private String department;
+    private int phoneNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFacultyNo() {
+        return facultyNo;
+    }
+
+    public void setFacultyNo(int facultyNo) {
+        this.facultyNo = facultyNo;
+    }
+
+    public char getAddress() {
+        return address;
+    }
+
+    public void setAddress(char address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
